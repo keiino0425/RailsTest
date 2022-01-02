1cad891bb72bfe64b550ef22c172732926d8c613
keiino <ineedayy0425@gmail.com>
2022-01-03 08:31:47 +0900

    create model of user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+       #以下の:name部分は追加したカラム名に変える
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :userimage, :userinfo]
+    end
 end
